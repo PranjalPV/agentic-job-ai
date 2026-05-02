c03b378f93ad85f30f36e250c0a8de841ca3ed5a
PranjalPV <93711960+PranjalPV@users.noreply.github.com>
2026-05-03 05:10:37 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -25,9 +25,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://agentic-job-ai.netlify.app"
     ],
-    allow_origin_regex=r"https://.*\.netlify\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
